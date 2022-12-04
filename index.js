be9ffe4788456c82fe45c5ca1a7c4f0f88d372fd
Felicity0410 <felicityliu0410@gmail.com>
2022-12-04 14:49:18 +1030

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import './index.css'
+
+import { profile, highline, card } from './src/data/db.json'
+
+import { 
+  navBar,
+ nav_container,
+ nav_container_logo,
+ nav_container_searchBar,
+ nav_container_create,
+ nav_container_create_img,
+ left_profile_img,
+ left_profile_name, 
+ left_profile_at,
+ middle_highline_description,
+ middle_highline_description_img,
+ middle_highline_description_p,
+ middle_form_img,
+ middle_form_input_text,
+ create_news,
+ create_profile_structure,
+ create_picture_structure,
+ create_control_structure,
+ create_comment_structure
+} from '@/js/common' 
+
+// Nav
+nav_container_logo.textContent ='TopSocial'
+nav_container_create_img.src = profile.img_src
+
+// Left 
+left_profile_img.src = profile.img_src
+left_profile_name.textContent = profile.name
+left_profile_at.textContent = profile.at 
+
+// Middle
+
+// Form
+middle_form_img.src = profile.img_src
+middle_form_input_text.placeholder = `What is on your mind, ${profile.name} ?`
+
+// Highline
+middle_highline_description_p.forEach((element, index) => {
+  element.textContent = highline.description[index]
+})
+middle_highline_description_img.forEach((element, index) => {
+  element.src = `./src/images/profile/profile_${index + 2}.png`
+})
+middle_highline_description.forEach((element, index) => {
+  element.style.backgroundImage = `url("./src/images/highline/highline_${index + 1}.jpeg")`
+})
+
+// Card
+const cardDom = document.querySelector('.card')
+
+card.forEach(element => {
+  const doms = create_news()
+  cardDom.appendChild(doms.info)
+
+  create_profile_structure(doms.info_profile, element.profile)
+
+  create_picture_structure(doms.info_picture, element.picture)
+
+  create_control_structure(doms.info_control)
+
+  create_comment_structure(doms.info_comment, element.comment)
+})
+
+
